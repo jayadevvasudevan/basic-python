76c123bb5fb88542a963668b41243220b2e3c3de
Hathaipach <hathaipach@hotmail.com>
2024-10-10 21:24:09 +0700

    Create Readingwriting.py
    Create Readingwriting.py

---
+++ Readingwriting.py
@@ -0,0 +1,8 @@
+# Writing to a file
+with open("example.txt", "w") as file:
+    file.write("Hello, World!")
+
+# Reading from a file
+with open("example.txt", "r") as file:
+    content = file.read()
+    print(content)
